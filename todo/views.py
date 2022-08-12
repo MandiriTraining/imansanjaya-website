250212b903b70650e05870a9ce45a79aa3017bbe
MandiriTraining <111072208+MandiriTraining@users.noreply.github.com>
2022-08-12 10:10:33 +0700

    push6

--- todo/views.py
+++ todo/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Belajar Django Luuur")
+    # return HttpResponse("Belajar Django Luuur")
+    return render(request, 'todo/index.html')
